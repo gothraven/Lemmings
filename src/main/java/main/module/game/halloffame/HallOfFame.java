327940db25f527e3d5278172c9b4c5a81da2596f
GothRaven <szaghbane@gmail.com>
2017-12-12 14:07:55 +0100

    working on saving the high scores of players

--- src/main/java/main/module/game/halloffame/HallOfFame.java
+++ src/main/java/main/module/game/halloffame/HallOfFame.java
@@ -6,47 +6,49 @@ import java.io.*;
 import java.util.Stack;
 
 public class HallOfFame {
-    private static String PATH = "high scores/high scores.db";
-    private Stack<Player> database;
+	private static String PATH = "high scores/high scores.db";
+	private Stack<Player> database;
 
-    public HallOfFame () {
-        this.database = new Stack<Player>();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream(PATH)));
-        this.loadDataBase(bufferedReader);
-    }
+	public HallOfFame () {
+		this.database = new Stack<Player>();
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream(PATH)));
+		this.loadDataBase(bufferedReader);
+	}
 
-    public static void main (String[] args) {
-        HallOfFame hf = new HallOfFame();
-        System.out.println(hf.getDatabase().peek().getName());
-        System.out.println(hf.getDatabase().peek().getScore());
-    }
+	public void addPlayer (Player player) {
+		if (isHighScore(player.getScore())) {
+			this.database.push(player);
 
-    public void addPlayer(Player player) {
-        if (isHighScore(player.getScore())) {
-            this.database.push(player);
-            //TODO add the player to the high score chart
-        }
+			try {
+				String newHighScore = player.getName() + "	" + player.getScore();
+				FileWriter fileWriter = new FileWriter(ClassLoader.getSystemResource(PATH).toString(), true);
+				//fileWriter.write(newHighScore);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
-    }
+	}
 
-    private boolean isHighScore(int score) {
-        return database.peek().getScore() < score;
-    }
+	private boolean isHighScore (int score) {
+		return database.peek().getScore() < score;
+	}
 
-    public Stack<Player> getDatabase () {
-        return database;
-    }
+	public Stack<Player> getDatabase () {
+		return database;
+	}
 
-    private void loadDataBase(BufferedReader bufferedReader) {
-        try {
-            String line;
-            while ((line = bufferedReader.readLine()) != null){
-                String[] player = line.split("  ");
+	private void loadDataBase (BufferedReader bufferedReader) {
+		/*try {
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				String[] player = line.split("	");
 
-                this.database.push(new Player(player[0], 123));
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+				this.database.push(new Player(player[0], Integer.parseInt(player[1])));
+			}
+			bufferedReader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
+	}
 }
