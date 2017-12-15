037789ef556d83d8dca63e101d32dbc3d4789951
Safiy Errahmane Zaghbane <szaghbane@gmail.com>
2017-12-15 10:51:38 +0100

    using maven plugin and setting up the project test and resources

--- src/main/java/main/module/game/level/Level.java
+++ src/main/java/main/module/game/level/Level.java
@@ -1,17 +1,22 @@
 package main.module.game.level;
 
+import main.module.game.Game;
+
 import java.io.InputStreamReader;
 
 public class Level {
 
-	//TODO Game
+	private Game game;
 	//TODO LevelInfo
 	//TODO Lemmings
 	//TODO Map
 
 	public Level (InputStreamReader levelFile) {
-		//TODO consturc the level based on a inputStream of a level file given in paramaters
-		// throw an exception in case the file is not a level file or null
+		this(null, levelFile);
+	}
+
+	public Level (Game game, InputStreamReader levelFIle) {
+		this.game = game;
 	}
 
 	public void update () {
