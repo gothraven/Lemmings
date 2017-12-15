92577be0937d740c090a164e796f874965fbdedf
GothRaven <szaghbane@gmail.com>
2017-12-15 23:05:26 +0100

    now the tile can do actions based on the map or the lemmings

--- src/main/java/main/module/game/level/factory/map/Tile.java
+++ src/main/java/main/module/game/level/factory/map/Tile.java
@@ -1,11 +1,19 @@
 package main.module.game.level.factory.map;
 
+import main.module.game.level.factory.lemming.Lemming;
+
 public class Tile {
 
 	private TileType type;
+	private TileAction action;
 
 	public Tile(TileType type) {
 		this.type = type;
+		this.action = type.getAction();
+	}
+
+	public void action(Lemming lemming, Map map) {
+		this.action.action(lemming, map);
 	}
 
 	public TileType getType () {
