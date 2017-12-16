06be3a55445776ccc79b921274f2ad7ed5072f10
noukaza <noukaza@gmail.com>
2017-12-16 10:40:35 +0100

    state lemming walker

--- src/main/java/main/util/power/PowerRules.java
+++ src/main/java/main/util/power/PowerRules.java
@@ -3,9 +3,12 @@ package main.util.power;
 
 import main.module.game.level.factory.lemming.Lemming;
 import main.module.game.level.factory.map.Map;
+import main.util.geometry.Direction;
+import main.util.geometry.Position;
 
 public interface PowerRules {
 
     void action (Lemming lem, Map map);
+
 
 }
