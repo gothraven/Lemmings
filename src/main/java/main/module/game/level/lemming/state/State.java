110857ead7e997d11862e07cbeaf253e31b75c35
szaghban <szaghbane@gmail.com>
2017-12-19 19:17:23 +0100

    we should rework the lemmings as faast as possible cause its the last thing we need to impliment and its the most important thing

--- src/main/java/main/module/game/level/lemming/state/State.java
+++ src/main/java/main/module/game/level/lemming/state/State.java
@@ -14,14 +14,14 @@ public enum State implements PowerRules {
 
 	WALKER {
 		public void action (Lemming lem, Map map, ArrayList<Lemming> lems) {
-			if(shouldFall(lem.getPos(),map) && lem.getPower() == State.WALKER){
+			if (shouldFall(lem.getPos(), map) && lem.getPower() == State.WALKER) {
 				lem.fall();
 				lem.fallingCounter();
-			}else{
-				if (shouldMove(lem,map)) {
+			} else {
+				if (shouldMove(lem, map)) {
 					lem.walk();
-				}else {
-					if(shouldJump(lem,map))
+				} else {
+					if (shouldJump(lem, map))
 						lem.jump();
 					else
 						lem.oppositDirection();
@@ -30,8 +30,8 @@ public enum State implements PowerRules {
 			}
 			if (lem.getFallingCounter() >= Lemming.MAX_HEIGHT)
 				lem.kill();
-			if(amIsomewhere(lem,map))
-				iAmSomewhere(lem,map);
+			if (amIsomewhere(lem, map))
+				iAmSomewhere(lem, map);
 		}
 	},
 	BOMBER {
@@ -42,16 +42,16 @@ public enum State implements PowerRules {
 	},
 	BLOCKER {
 		public void action (Lemming lem, Map map, ArrayList<Lemming> lems) {
-			if (shouldFall(lem.getPos() ,map)) {
+			if (shouldFall(lem.getPos(), map)) {
 				lem.fall();
 				lem.fallingCounter();
-			}else{
+			} else {
 				lem.restFallingCounter();
 			}
 			if (lem.getFallingCounter() >= Lemming.MAX_HEIGHT)
 				lem.kill();
-			if(amIsomewhere(lem,map))
-				iAmSomewhere(lem,map);
+			if (amIsomewhere(lem, map))
+				iAmSomewhere(lem, map);
 		}
 	},
 	BUILDER {
@@ -62,9 +62,9 @@ public enum State implements PowerRules {
 	CLIMBER {
 		public void action (Lemming lem, Map map, ArrayList<Lemming> lems) {
 			Tile tDirection = map.getTileInThisDirection(lem.getPos(), lem.getDir());
-			if(tDirection == null){
-				WALKER.action(lem,map,lems);
-			}else{
+			if (tDirection == null) {
+				WALKER.action(lem, map, lems);
+			} else {
 
 				lem.moveUp();
 			}
@@ -75,10 +75,10 @@ public enum State implements PowerRules {
 		public void action (Lemming lem, Map map, ArrayList<Lemming> lems) {
 
 			Tile TDown = map.getTileInThisDirection(lem.getPos(), Direction.DOWN);
-			if(TDown.getType().isDestructible()){
+			if (TDown.getType().isDestructible()) {
 				//TODO don't forget
-			}else{
-				State.WALKER.action(lem,map,lems);
+			} else {
+				State.WALKER.action(lem, map, lems);
 			}
 			if (countDigger == 6) {
 				lem.changePower(State.WALKER);
@@ -88,13 +88,13 @@ public enum State implements PowerRules {
 	},
 	MINER {
 		public void action (Lemming lem, Map map, ArrayList<Lemming> lems) {
-			if(shouldMove(lem , map) || shouldFall(lem.getPos(),map)){
-				State.WALKER.action(lem,map,lems);
-			}else{
+			if (shouldMove(lem, map) || shouldFall(lem.getPos(), map)) {
+				State.WALKER.action(lem, map, lems);
+			} else {
 				Tile tDirection = map.getTileInThisDirection(lem.getPos(), lem.getDir());
-				if(tDirection.getType().isDestructible()){
-					map.removeTile(new Position(lem.getPos().getX()+lem.getDir().getXdir(),lem.getPos().getY()+lem.getDir().getYdir()));
-				}else {
+				if (tDirection.getType().isDestructible()) {
+					map.removeTile(new Position(lem.getPos().getX() + lem.getDir().getXdir(), lem.getPos().getY() + lem.getDir().getYdir()));
+				} else {
 					lem.oppositDirection();
 					lem.changePower(State.WALKER);
 				}
@@ -102,23 +102,22 @@ public enum State implements PowerRules {
 		}
 	},
 	PARATROOPER {
-
 		public void action (Lemming lem, Map map, ArrayList<Lemming> lems) {
 
-			if(shouldFall(lem.getPos(),map)){
-				if (isFlying){
+			if (shouldFall(lem.getPos(), map)) {
+				if (isFlying) {
 					lem.fall();
 					lem.fallingCounter();
 					isFlying = false;
-				}else{
+				} else {
 					isFlying = true;
 				}
 
-			}else{
-				if (shouldMove(lem,map)) {
+			} else {
+				if (shouldMove(lem, map)) {
 					lem.walk();
-				}else {
-					if(shouldJump(lem,map))
+				} else {
+					if (shouldJump(lem, map))
 						lem.jump();
 					else
 						lem.oppositDirection();
@@ -128,44 +127,45 @@ public enum State implements PowerRules {
 
 			if (lem.getFallingCounter() >= Lemming.MAX_HEIGHT)
 				lem.kill();
-			if(amIsomewhere(lem,map))
-				iAmSomewhere(lem,map);
+			if (amIsomewhere(lem, map))
+				iAmSomewhere(lem, map);
 		}
 	};
 
 	boolean isFlying = false;
 	int countDigger = 0;
 
-	protected boolean shouldFall(Position pos, Map map){
+	protected boolean shouldFall (Position pos, Map map) {
 		return map.getTile(new Position(pos.getX() + Direction.DOWN.getXdir(), pos.getY() + Direction.DOWN.getYdir())) == null;
 
 	}
 
-	protected boolean shouldMove(Lemming lem, Map map){
+	protected boolean shouldMove (Lemming lem, Map map) {
 
 		Tile tDirection = map.getTileInThisDirection(lem.getPos(), lem.getDir());
 		return tDirection == null || tDirection.getType() == TileType.ENTER || tDirection.getType() == TileType.BOMB || tDirection.getType() == TileType.EXIT || tDirection.getType() == TileType.LAVA || tDirection.getType() == TileType.TELEPORT;
 	}
 
-	protected boolean shouldJump(Lemming lem, Map map){
+	protected boolean shouldJump (Lemming lem, Map map) {
 		Tile tUp = map.getTileInThisDirection(lem.getPos(), Direction.UP);
 		Tile tDUpper = map.getTileInThisDirection(lem.getPos(), lem.getDir().upper());
 		return tUp == null & (tDUpper == null || tDUpper.getType() == TileType.BOMB || tDUpper.getType() == TileType.EXIT || tDUpper.getType() == TileType.TELEPORT || tDUpper.getType() == TileType.LAVA || tDUpper.getType() == TileType.ENTER);
 	}
 
-	protected boolean amIsomewhere(Lemming lem , Map map){
+	protected boolean amIsomewhere (Lemming lem, Map map) {
 		Tile tMe = map.getTile(lem.getPos());
 		return tMe != null;
 	}
 
-	protected void iAmSomewhere(Lemming lem, Map map){
+	protected void iAmSomewhere (Lemming lem, Map map) {
 		Tile tMe = map.getTile(lem.getPos());
-		if(tMe.getType() == TileType.EXIT)
+		if (tMe.getType() == TileType.EXIT)
 			lem.save();
-		else if(tMe.getType() == TileType.LAVA)
+		else if (tMe.getType() == TileType.LAVA)
 			lem.kill();
-		else if(tMe.getType() == TileType.TELEPORT)
+		else if (tMe.getType() == TileType.TELEPORT)
 			//TODO add function to Teleport
+			//TODO tu peux utiliser ici tMe.action(lem, map, null);
 			System.out.println("TELEPORT");
 		else if (tMe.getType() == TileType.BOMB)
 			//TODO add function explose
